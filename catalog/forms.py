21bbf6fb7ab7333e0fdb37f3525039807119497a
Эльвира Чернова <gr435_cheeni@lab-402-6.tomtit-tomsk.ru>
2025-09-30 17:29:36 +0700

    Валидация и попревка форм

--- catalog/forms.py
+++ catalog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth import password_validation
 from django.core.exceptions import ValidationError
 from .models import AdvUser
-
+from django.core.validators import RegexValidator
 
 class ChangeUserInfoForm(forms.ModelForm):
    email = forms.EmailField(required=True, label='Адрес электронной почты')
@@ -12,18 +12,46 @@ class ChangeUserInfoForm(forms.ModelForm):
        fields = ('username', 'email', 'fio')
 
 class RegisterUserForm(forms.ModelForm):
-    email = forms.EmailField(required=True, label='Адрес электронной почты')
+    email = forms.EmailField(required=True, label='Адрес электронной почты', validators=[
+        RegexValidator(
+            regex=r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',
+            message=' Необходим валидный формат email-адреса'
+        )
+    ])
 
     password1 = forms.CharField(
         label='Пароль',
-        widget=forms.PasswordInput,help_text=password_validation.password_validators_help_text_html()
+        widget=forms.PasswordInput,
+
     )
     password2 = forms.CharField(
         label='Пароль (повторно)',
         widget=forms.PasswordInput,help_text='Повторите тот же самый пароль еще раз'
     )
-    fio = forms.CharField(label='ФИО', max_length=100)
-    username = forms.CharField(label='Логин', max_length=30)
+    first_name = forms.CharField(label='Имя', max_length=100, validators=[
+        RegexValidator(
+            regex=r'^[а-яА-ЯёЁ\s-]+$',
+            message='Имя должно состоять только из кириллических букв, пробелов и дефисов.'
+        )
+    ])
+    last_name = forms.CharField(label='Фамилия', max_length=100, validators=[
+        RegexValidator(
+            regex=r'^[а-яА-ЯёЁ-]+$',
+            message='Фамилия должно состоять только из кириллических букв и дефисов.'
+        )
+    ])
+    username = forms.CharField(label='Логин', max_length=30, validators=[
+        RegexValidator(
+            regex=r'^[a-zA-Z-]+$',
+            message='Логин должен состоять только из латинских букв и дефисов.'
+        )
+    ])
+    patronymic = forms.CharField(label='Отчество', max_length=100, validators=[
+        RegexValidator(
+            regex=r'^[а-яА-ЯёЁ-]+$',
+            message='Отчество должно состоять только из кириллических букв и дефисов.'
+        )
+    ])
     consent = forms.BooleanField(label='Согласие на обработку персональных данных')
 
     def clean_password1(self):
@@ -54,4 +82,4 @@ class RegisterUserForm(forms.ModelForm):
 
     class Meta:
         model = AdvUser
-        fields = ('username', 'fio', 'email', 'password1', 'password2', 'consent')
+        fields = ('username', 'first_name', 'patronymic', 'last_name', 'email', 'password1', 'password2', 'consent')
